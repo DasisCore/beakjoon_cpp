ed675f38329c630d7e4b485ce9982d6b26dee433
Heojaeyoung <gj30347896@gmail.com>
2022-06-22 18:21:05 +0900

    22-06-22

--- src/practice.cpp
+++ src/practice.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
+void recur(int cur) {
+
+    if (cur == 7) {
+        return;
+    }
+
+    cout << cur;
+
+    return recur(cur + 1);
+}
 
 int main() {
 
-    int map[4][4] = {
-        6, 6, 5, 4,
-        1, 2, 3, 4,
-        1, 1, 1, 1,
-        5, 4, 3, 2
-    };
+    int a;
 
-    int x , y;
-    cin >> x >> y;
-
-    int sum = 0;
-    sum += map[x - 1][y + 0]; // 왼쪽
-    sum += map[x + 1][y + 0]; // 오른쪽
-    sum += map[x + 0][y - 1]; // 아래
-    sum += map[x + 0][y + 1]; // 위
-
-    // 이 단순 반복 코드를 for문을 이용해서 정리해보자.
-
-    cout << sum;
+    cin >> a;
+    recur(a);
     
-    
-    
-    
-
     return 0;
 }
