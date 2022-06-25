ae7087475fcd7cda999d494bd3310eb3608d48b8
Heojaeyoung <gj30347896@gmail.com>
2022-06-25 17:28:54 +0900

    22-06-25

--- src/practice.cpp
+++ src/practice.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-// 规巩 眉农侩 vector
-vector<vector<int>> v(20);
-
-void dfs(int now) {
-    for(int i = 0; i < v[now].size(); i++) {
-        int to = v[now][i];
-        cout << to;
-        dfs(to);
-    }
-}
 
 int main() {
 
-    int n;
+    int n, p;
+    cin >> n >> p;
 
-    cin >> n;
+    int cnt = 0;
+    int x = p;
 
-    for(int i = 0; i < n - 1; i++) {
-        int a, b;
-        cin >> a >> b;
-        v[a].push_back(b);
-    }; // 牢立 府胶飘 积己
+    while (x <= n) {
+        cnt += n / x;
+        x *= p; 
+    }
 
-    dfs(n);
-
-    int de = 1;
-    
-    return 0;
+    cout << cnt;
 }
