b029f24e50e187dc5ef37d861be4d746ea055570
Heojaeyoung <gj30347896@gmail.com>
2022-06-30 22:45:47 +0900

    22-06-30

---
+++ src/0630/recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int fact = 1;
+void recur(int n) {
+
+    if(n == 1) {
+        cout << fact;
+        return;
+    }
+
+    fact *= n;
+    recur(n - 1);
+
+}
+
+int main(){
+    recur(5);
+    return 0;
+}
